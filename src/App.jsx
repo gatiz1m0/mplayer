50b45b78907b53752ec7069b4cf354f5ab1ba654
Glitch (hello-cra) <none>
2021-02-15 20:19:13 +0000

    🏭🎎 Checkpoint ./src/App.jsx:13541750/5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import BreakInterval from '/components/BreakInterval'
+import BreakInterval from './components/BreakInterval'
 
 class App extends React.Component {
   constructor() {
