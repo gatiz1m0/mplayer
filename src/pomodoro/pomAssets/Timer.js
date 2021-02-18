6489829d6f8633c337eefe1867938931b9131bcb
Glitch (lando-pomodoro) <none>
2021-02-18 23:13:22 +0000

    🏭🍙 Checkpoint ./src/pomodoro/pomAssets/Timer.js:13541750/1225

--- src/pomodoro/pomAssets/Timer.js
+++ src/pomodoro/pomAssets/Timer.js
@@ -8,20 +8,45 @@ class Timer extends React.Component {
     
     this.state = {
       inSession: true,
-      timerSeconds: 0
+      timerSeconds: 0,
+      intervalId: 0
     }
     
-    
-    
+    const playSymbol = "▶";
+    const pauseSymbol ="❚❚";
+    let symbol = playSymbol;
+
     this.play = this.play.bind(this)
     this.reset = this.reset.bind(this)
+    this.decreaseTimer = this.decreaseTimer.bond(this)
+  }
+  
+  play() {
+    let intervalId = setInterval(this.decreaseTimer, 1000);
+    
+    this.setState({
+      intervalId: intervalId
+    })
+  }
+  
+  decreaseTimer() {
+    switch(this.state.timerSeconds) {
+      case 0:
+        this.setState( (prevState) => {
+          return 
+        })
+    }
+  }
+
+  reset() {
+
   }
   
   render() {
     return(
       <section>       
         <div className="timer-area">
-          <button onClick={this.play}>▶❚❚</button>
+          <button onClick={this.play}>{this.symbol}</button>
           <section className="timer">
           <h4 className="session">{this.state.isSession === true ?
               "Session" : "Break"}</h4>
