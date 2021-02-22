1758a64d0e25a62248c423efd7df5c2333c0de54
Glitch (lando-pomodoro) <none>
2021-02-22 00:12:15 +0000

    👕🚇 Checkpoint ./src/musicplayer/MPlayer.js:13541750/947 ./.glitch-assets:13541750/397 ./src/musicplayer/MPlayer.css:13541750/772

--- src/musicplayer/MPlayer.js
+++ src/musicplayer/MPlayer.js
@@ -7,18 +7,23 @@ const MPlayer = () => {
     {
       title: "Song 1",
       artist: "from Soundstripe",
-      art: "",
-      src: ""
+      artSrc: "https://cdn.glitch.com/7c6c6fcd-6b82-4a23-8d7a-b681e58e72ea%2Fmaxresdefault.jpg?v=1613952236992",
+      trackSrc: "https://cdn.glitch.com/7c6c6fcd-6b82-4a23-8d7a-b681e58e72ea%2FSong1.mp3?v=1613951915184"
     },
     {
-      
+     title: "Song 2",
+      artist: "from Soundstripe",
+      artSrc: "https://cdn.glitch.com/7c6c6fcd-6b82-4a23-8d7a-b681e58e72ea%2Fmaxresdefault.jpg?v=1613952236992",
+      trackSrc: "https://cdn.glitch.com/7c6c6fcd-6b82-4a23-8d7a-b681e58e72ea%2FSong2.mp3?v=1613951927783"
     }
   ])
   
   return (
     <div className='mplayer'>
-    
-    </div>
+      <div className="main">
+     <p className="title">Study&Chill</p>
+      </div>
+    </div>div>
   )
 }
 
